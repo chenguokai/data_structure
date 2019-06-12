7965682112016f22042d38e21211f1dea39f506f
chenguokai <1415430544@qq.com>
2019-06-12 22:16:38 +0800

    GUI initialization

--- traffic-gui/mainwindow.cpp
+++ traffic-gui/mainwindow.cpp
@@ -5,10 +5,76 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    //ui->setupUi(this);
+    initUI();
+    initSlots();
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::initUI() {
+    from_station = new QComboBox;
+    to_station = new QComboBox;
+    from_station->setEditable(true);
+    to_station->setEditable(true);
+    from_list = new QListWidget;
+    to_list = new QListWidget;
+    from_station->setModel(from_list->model());
+    to_station->setModel(to_list->model());
+    from_station->setView(from_list);
+    to_station->setView(to_list);
+    for (int i = 0; i < 5; ++i)
+        {
+            from_station->addItem("item " + QString::number(i));
+        }
+    inputview = new QHBoxLayout;
+    from_des = new QLabel;
+    to_des = new QLabel;
+    from_des->setText("出发地");
+    to_des->setText("目的地");
+
+    inputview->addWidget(from_des);
+    inputview->addWidget(from_station);
+    inputview->addWidget(to_des);
+    inputview->addWidget(to_station);
+
+    mainview = new QVBoxLayout;
+
+    mode_des = new QLabel;
+    mode_des->setText("选择策略");
+    mode = new QComboBox;
+    mode_list = new QListWidget;
+    mode->setModel(mode_list->model());
+    mode->setView(mode_list);
+
+    modeview = new QHBoxLayout;
+    modeview->addWidget(mode_des);
+    modeview->addWidget(mode);
+
+    result = new QPlainTextEdit();
+    result->setFixedHeight(200);
+    result->setReadOnly(true);
+    result->setPlainText("This is a test string");
+
+    kickstart = new QPushButton;
+    kickstart->setText("开始寻路");
+    kickview = new QHBoxLayout;
+    kickview->addSpacing(100);
+    kickview->addWidget(kickstart);
+
+    mainview->addLayout(inputview);
+    mainview->addLayout(modeview);
+    mainview->addWidget(result);
+    mainview->addLayout(kickview);
+    auto central = new QWidget;
+    central->setLayout(mainview);
+    setCentralWidget(central);
+}
+
+void MainWindow::initSlots() {
+
 }
--- traffic-gui/mainwindow.h
+++ traffic-gui/mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPlainTextEdit>
+#include <QTextEdit>
+#include <QComboBox>
+#include <QListWidget>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QPushButton>
 
 namespace Ui {
 class MainWindow;
@@ -14,6 +21,23 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void initUI();
+    void initSlots();
+    QComboBox* from_station;
+    QComboBox* to_station;
+    QComboBox* mode;
+    QListWidget* from_list;
+    QListWidget* to_list;
+    QListWidget* mode_list;
+    QLabel *from_des,*to_des,*mode_des;
+    QHBoxLayout* inputview;
+    QVBoxLayout* mainview;
+    QHBoxLayout* modeview;
+    QPlainTextEdit* result;
+    QPushButton* kickstart;
+    QHBoxLayout* kickview;
+
+
 
 private:
     Ui::MainWindow *ui;
