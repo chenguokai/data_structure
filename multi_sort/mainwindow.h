3b47f7a58e211b82cafa32d4dfe849cb38dc8601
chenguokai <1415430544@qq.com>
2019-06-16 14:47:32 +0800

    Prj2: finished GUI design

--- multi_sort/mainwindow.h
+++ multi_sort/mainwindow.h
@@ -2,6 +2,18 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QFileDialog>
+#include <QTextEdit>
+#include <QPlainTextEdit>
+#include <QComboBox>
+#include <QWidgetList>
+#include <QListWidget>
+#include <QCheckBox>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QLineEdit>
+#include <QPushButton>
 
 namespace Ui {
 class MainWindow;
@@ -14,6 +26,34 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void initUI();
+    void initSlots();
+    QLabel* open_file_des;
+    QFileDialog* open_file;
+    QLabel* sort1_des;
+    QLabel* sort2_des;
+    QLabel* sort3_des;
+    QLabel* sort4_des;
+    QLabel* sort5_des;
+    QComboBox* sort1_input;
+    QComboBox* sort2_input;
+    QComboBox* sort3_input;
+    QComboBox* sort4_input;
+    QComboBox* sort5_input;
+    QListWidget* sort1_input_list;
+    QListWidget* sort2_input_list;
+    QListWidget* sort3_input_list;
+    QListWidget* sort4_input_list;
+    QListWidget* sort5_input_list;
+    QCheckBox* name_included;
+    //QLabel* name_included_des;
+    QPlainTextEdit* res;
+    QLineEdit* path;
+    QHBoxLayout* file_view;
+    QHBoxLayout* sort_input_view;
+    QHBoxLayout* name_view;
+    QVBoxLayout* main_view;
+    QPushButton* kickstart;
 
 private:
     Ui::MainWindow *ui;
